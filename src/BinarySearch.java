67695a09de3cfdff16b6236e4d2b9766da7aa149
vinod prajapati <vk7444741@gmail.com>
2024-10-16 11:30:29 +0530

    java Array

---
+++ src/BinarySearch.java
@@ -0,0 +1,33 @@
+public class BinarySearch {
+    static int binarySearch(int []arr , int x){
+        int low =0;
+        int high = arr.length-1;
+        while (low<=high){
+            int mid = low+(high-low);
+            // check x is present in mid
+            if(arr[mid]==x)
+                return mid;
+            // check x is greater than mid
+            if (arr[mid]<x)
+                low = mid+1;
+            // check x is smaller the mid
+           // if (arr[mid]>x)
+            else
+                high = mid-1;
+        }
+        //if we reach here than the element not present
+        return -1;
+    }
+    public static void main(String[] args) {
+       BinarySearch vinod =  new BinarySearch();
+        int [] arr = {2,3,4,10,40,50,60};
+        int n = arr.length;
+        int x = 50;
+        int result = vinod.binarySearch(arr ,x);
+        if (result ==-1)
+            System.out.println("Element is not present");
+        else
+            System.out.println("element is present "+ result);
+    }
+
+}
--- src/findElement.java
+++ src/findElement.java
@@ -1,29 +1,47 @@
 import java.util.Scanner;
 public class findElement {
+    public static int search(int []arr , int n, int target){
+        for (int i=0; i<n; i++){
+            if (arr[i]== target)
+                return i; // sout(i for print index) and sout(arr[i] for print element
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.println("Enter the target number");
         int target = sc.nextInt();
         System.out.println("enter the size of array");
         int n = sc.nextInt();
-
+        System.out.println("please Enter the Array");
         int [] arr = new int [n];
-        int len = arr.length;
-        System.out.println("enter the array element");
-        for (int i =0; i<len; i++){
+        for (int i=0; i<arr.length; i++){
             arr[i] = sc.nextInt();
         }
-        boolean flag = false; // false means element not found
-        for (int i = 0; i<len; i++) {
-            if (arr[i] == target) {
-                flag = true;
-                break;
-            }
-        }
-            if (flag == true ){
-                System.out.println("element found ");
-            }else{
-                System.out.println("element not found");
-            }
+        // function call
+        int res = search(arr,arr.length, target);
+        if (res == -1)
+            System.out.println("Element is not present");
+        else
+            System.out.println("Element is present" + " "+res);
+
+//        int [] arr = new int [n];
+//        int len = arr.length;
+//        System.out.println("enter the array element");
+//        for (int i =0; i<len; i++){
+//            arr[i] = sc.nextInt();
+//        }
+//        boolean flag = false; // false means element not found
+//        for (int i = 0; i<len; i++) {
+//            if (arr[i] == target) {
+//                flag = true;
+//                break;
+//            }
+//        }
+//            if (flag == true ){
+//                System.out.println("element found ");
+//            }else{
+//                System.out.println("element not found");
+//            }
     }
 }
